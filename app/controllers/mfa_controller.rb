b7181f25f4b4979c3bec59b93aa933b2e5eef8e3
hailong <hailong.zheng@classi.jp>
2019-12-17 15:50:42 +0900

    認証端末登録

--- app/controllers/mfa_controller.rb
+++ app/controllers/mfa_controller.rb
@@ -1,39 +1,40 @@
 class MfaController < ApplicationController
   def index
-    if user_id = session[:before_mfa_user_id]
-      @user = User.find_by(id: user_id)
-      # デバイス設定
-      secret = ROTP::Base32.random
-      p secret
-
-      totp = ROTP::TOTP.new(secret, issuer: "two-factor_sample")
-      uri = totp.provisioning_uri("hailong@test.jp")
-
-      @qr = RQRCode::QRCode.new(uri, level: :h)
-    else
-      redirect_to login_url
+    unless user = User.find_by(id: session[:before_mfa_user_id])
+      return redirect_to login_url
     end
+
+    # if user.otp_secret_key.present?
+    #   user.otp_secret_key
+    #   return render "new"
+    # end
+
+    # 認証デバイス登録
+    unless user.otp_secret_key.present?
+      secret = ROTP::Base32.random
+      user.update(otp_secret_key: secret)
+    end
+
+    totp = ROTP::TOTP.new(user.otp_secret_key, issuer: "two-factor_sample")
+    uri = totp.provisioning_uri("hailong@test.jp")
+    @qr = RQRCode::QRCode.new(uri, level: :h)
   end
 
   def new
   end
 
   def create
-    if user_id = session[:before_mfa_user_id]
-      user = User.find_by(id: user_id)
-
-      # テスト確認用
-      secret = "63TMGOXBMARHFDZOMNEWPAOYEAJ4UEZ4"
-      totp = ROTP::TOTP.new(secret, issuer: "two-factor_sample")
+    if user = User.find_by(id: session[:before_mfa_user_id])
+      totp = ROTP::TOTP.new(user.otp_secret_key, issuer: "two-factor_sample")
       if totp.verify(params[:otp_code])
         log_in user
-        return redirect_to root_url, notice: "Loged in!"
+        return redirect_to root_url, notice: "認証成功しました!"
       else
         flash.now[:alert] = "認証コードは無効です。"
         return render "new"
       end
     else
-      return redirect_to root_url, notice: "ログイン失敗!"
+      return redirect_to root_url, notice: "ログイン失敗しました!"
     end
   end
 end
